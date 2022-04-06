a283e8e3b2a98248751baeed06052ecaed6a1bb4
roybogin <roybog00@gmail.com>
2022-04-07 00:28:36 +0300

    changed perpendicular - return None if intersection not on segment

--- scan_to_map.py
+++ scan_to_map.py
@@ -1,16 +1,24 @@
-
-
 from math import sqrt
 
 
 def perpendicularDistance(point, start_point, end_point):
-    x0 = start_point[0]
-    y0 = start_point[1]
-    m = (y0-end_point[1])/(x0-end_point[0])
-    n = y0 - m*x0
-    d = (m*point[0]-point[1]+n)/sqrt(m ** 2 + 1)
-    return abs(d)
-
+    x0, y0 = start_point
+    x1, y1 = end_point
+    x, y = point
+    if x0 == x1:
+        if min(y0, y1) <= y <= max(y0,y1):
+            return abs(x-x0)
+        return None
+    if y0 == y1:
+        if min(x0, x1) <= x <= max(x0,x1):
+            return abs(y-y0)
+        return None
+    m = (y0-y1)/(x0-x1)
+    inter_x = (y-y0 + m*x0 + x / m)/(m+1/m)
+    inter_y = y0 + m*(inter_x-x0)
+    if min(x0,x1) <= inter_x <= max(x0,x1):
+        return sqrt((x-inter_x)**2+(y-inter_y)**2)
+    return None
 
 
 
@@ -20,18 +28,15 @@ def points_to_map(points, epsilon):
     index = 0
     end = len(points)
     for i in range(1,end):
-        d = perpendicularDistance( points[i], start_point = points[0], end_point = points[-1]) 
-        if (len(points) == 6):
-            print(d)
+        d = perpendicularDistance(points[i], points[0], points[-1]) 
         if (d > dmax):
             index = i
             dmax = d
-    result = set()
 
     # If max distance is greater than epsilon, recursively simplify
     if (dmax > epsilon):
         # Recursive call
-        recResults1 = points_to_map(points[:index], epsilon)
+        recResults1 = points_to_map(points[:index+1], epsilon)
         recResults2 = points_to_map(points[index:], epsilon)
     # Build the result list
         result = recResults1 | recResults2
@@ -42,4 +47,4 @@ def points_to_map(points, epsilon):
     return result
 
 
-print(points_to_map([(1,0),(2,0),(3,1),(4,0),(5,0),(6,0)],0.9))
+print(points_to_map([(1,0),(2,0),(3,1),(4,0),(5,0),(6,0)],1))
