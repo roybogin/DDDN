715cfdece25a7708ced690c243ea79459b880f92
roybogin <roybog00@gmail.com>
2022-05-01 11:10:05 +0300

    very not ready RL - many bugs but firstly need to connect to car for running

--- RL.py
+++ RL.py
@@ -1,21 +1,60 @@
+from calendar import EPOCH
 import torch
-from torch import nn
+from torch import nn, optim
+from collections import namedtuple, deque
+from torch.optim.lr_scheduler import MultiStepLR
+
+import random
+import math
+import matplotlib.pyplot as plt
+from trainer import Trainer
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.linear_relu_stack = nn.Sequential(
+        self.lines_to_features = nn.Sequential(
             nn.Linear(4, 64),
             nn.ReLU(),
             nn.Linear(64, 128),
             nn.ReLU(),
             nn.Linear(128, 1024)
         )
+        self.get_features = torch.max
+        self.calculte_move = nn.Sequential(
+            nn.Linear(4+1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2),
+            nn.Tanh()
+        )
 
     def forward(self, x):
-        x = self.linear_relu_stack(x)
-        print(x, x.shape)
-        logits = torch.max(x, dim=1)
-        return logits
+        lines = x[2]
+        lines = self.lines_to_features(lines)
+        # maybe max_pool_2d kernel = [num, 1]
+        features = self.get_features(lines, dim=1)
+
+        move = self.calculte_move(x[0] + x[1] + features)
+        return move
+
+
+def main():
+    EPOCHS = 1000
+    model = NeuralNetwork()
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.MSELoss()
+    scheduler = MultiStepLR(optimizer, milestones=[30, 50], gamma=0.1)
+
+
+    trainer = Trainer(
+        model,
+        device,
+        EPOCHS,
+        criterion,
+        optimizer,
+        scheduler)
+    trainer.train()
---
+++ trainer.py
@@ -0,0 +1,122 @@
+"""Trainer.
+
+@author: Zhenye Na - https://github.com/Zhenye-Na
+@reference: "End to End Learning for Self-Driving Cars", arXiv:1604.07316
+"""
+
+import os
+import torch
+
+
+class Trainer(object):
+    """Trainer class."""
+
+    def __init__(self,
+                 model,
+                 device,
+                 epochs,
+                 criterion,
+                 optimizer,
+                 scheduler
+                 ):
+        """Self-Driving car Trainer.
+
+        Args:
+            model: CNN model
+            device: cuda or cpu
+            epochs: epochs to training neural network
+            criterion: nn.MSELoss()
+            optimizer: optim.Adam()
+            start_epoch: 0 or checkpoint['epoch']
+            trainloader: training set loader
+            validationloader: validation set loader
+
+        """
+        super(Trainer, self).__init__()
+
+        self.model = model
+        self.device = device
+        self.epochs = epochs
+        self.start_epoch = 0
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.scheduler = scheduler
+
+    def train(self):
+        """Training process."""
+        self.model.to(self.device)
+        for epoch in range(self.start_epoch, self.epochs + self.start_epoch):
+            self.scheduler.step()
+
+            # Training
+            train_loss = 0.0
+            self.model.train()
+
+            for local_batch, (centers, lefts, rights) in enumerate(self.trainloader):
+                # Transfer to GPU
+                centers, lefts, rights = toDevice(centers, self.device), toDevice(
+                    lefts, self.device), toDevice(rights, self.device)
+
+                # Model computations
+                self.optimizer.zero_grad()
+                datas = [centers, lefts, rights]
+                for data in datas:
+                    imgs, angles = data
+                    # print("training image: ", imgs.shape)
+                    outputs = self.model(imgs)
+                    loss = self.criterion(outputs, angles.unsqueeze(1))
+                    loss.backward()
+                    self.optimizer.step()
+
+                    train_loss += loss.data.item()
+
+                if local_batch % 100 == 0:
+
+                    print("Training Epoch: {} | Loss: {}".format(
+                        epoch, train_loss / (local_batch + 1)))
+
+            # Validation
+            self.model.eval()
+            valid_loss = 0
+            with torch.set_grad_enabled(False):
+                for local_batch, (centers, lefts, rights) in enumerate(self.validationloader):
+                    # Transfer to GPU
+                    centers, lefts, rights = toDevice(centers, self.device), toDevice(
+                        lefts, self.device), toDevice(rights, self.device)
+
+                    # Model computations
+                    self.optimizer.zero_grad()
+                    datas = [centers, lefts, rights]
+                    for data in datas:
+                        imgs, angles = data
+                        outputs = self.model(imgs)
+                        loss = self.criterion(outputs, angles.unsqueeze(1))
+
+                        valid_loss += loss.data.item()
+
+                    if local_batch % 100 == 0:
+                        print("Validation Loss: {}".format(
+                            valid_loss / (local_batch + 1)))
+
+            print()
+
+            # Save model
+            if epoch % 5 == 0 or epoch == self.epochs + self.start_epoch - 1:
+
+                print("==> Save checkpoint ...")
+
+                state = {
+                    'epoch': epoch + 1,
+                    'state_dict': self.model.state_dict(),
+                    'optimizer': self.optimizer.state_dict(),
+                    'scheduler': self.scheduler.state_dict(),
+                }
+
+                self.save_checkpoint(state)
+
+    def save_checkpoint(self, state):
+        """Save checkpoint."""
+        if not os.path.exists(self.ckptroot):
+            os.makedirs(self.ckptroot)
+
+        torch.save(state, self.ckptroot + 'model-{}.h5'.format(state['epoch']))
