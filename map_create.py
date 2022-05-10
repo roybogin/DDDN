5bdf1029cc2f0e275b34a4aa18af2cb9370edbf9
DCH <dchuparov@gmail.com>
2022-05-10 13:17:41 +0300

    fixed all - doesn't work for closed lines
    at the moment close polygonal lines are a problem, the produced walls overlap

--- map_create.py
+++ map_create.py
@@ -2,7 +2,7 @@ import math
 import pybullet as p
 import pybullet_data
 import os
-def create_wall(p, pos, orientation, length, width):
+def create_wall(pos, orientation, length, width):
 
 	boxHalfLength = length / 2
 	boxHalfWidth = width / 2
@@ -20,30 +20,63 @@ def create_wall(p, pos, orientation, length, width):
 	    pos,
 	    orientation,
 	)
+	return block
 
 def distance(p1, p2):
 	return math.sqrt(pow(p1[0]-p2[0],2) + pow(p1[1]-p2[1],2))
 
-def create_poly_wall(p, poly, epsilon):
+def create_poly_wall(poly, epsilon):
 	length = distance(poly[0], poly[1]) + 2 * epsilon
 	width = 2 * epsilon
-	angle = math.atan2(poly[1][1] - poly[0][1], poly[1][0] - poly[0][0])
-	euler = [0, 0, angle]
+	prev_angle = math.atan2(poly[1][1] - poly[0][1], poly[1][0] - poly[0][0])
+	euler = [0, 0, prev_angle]
 	orientation = p.getQuaternionFromEuler(euler)
 	pos = [(poly[0][0] + poly[1][0]) / 2, (poly[0][1] + poly[1][1]) / 2, 0.5]
-	create_wall(p, pos, orientation, length, width)
+	create_wall(pos, orientation, length, width)
+	walls = []
 	for i in range(1, len(poly) - 1):
-		length = distance(poly[0], poly[1]) + (1-math.sqrt(2))*epsilon
+		length = distance(poly[i], poly[i+1]) + epsilon
 		angle = math.atan2(poly[i+1][1] - poly[i][1], poly[i+1][0] - poly[i][0])
+		diff = angle - prev_angle
+		while diff > math.pi:
+			diff -= 2 * math.pi
+		while diff <= -math.pi:
+			diff += 2 * math.pi
 		euler = [0, 0, angle]
 		orientation = p.getQuaternionFromEuler(euler)
-		pos = [poly[i][0] + math.cos(angle) * (length / 2 + math.sqrt(2) * epsilon), poly[i][1] + math.sin(angle) * (length / 2 + math.sqrt(2) * epsilon), 0.5]
-		create_wall(p, pos, orientation, length, width)
+
+		length_from = 0
+		if diff == math.pi:
+			continue
+		if diff == 0 or diff == math.pi / 2 or diff == -math.pi/2:
+			length_from = epsilon
+
+		elif -math.pi / 2 < diff < math.pi / 2:
+			if diff > 0:
+				length_from = math.sqrt(2) * epsilon * cos(math.pi / 4 - diff)
+			else:
+				length_from = math.sqrt(2) * epsilon * cos(math.pi / 4 + diff)
+		else:
+			if diff > 0:
+				length_from += epsilon / math.tan(math.pi - diff)
+				length_from += epsilon / math.sin(math.pi - diff)
+			else:
+				length_from += epsilon / math.tan(math.pi + diff)
+				length_from += epsilon / math.sin(math.pi + diff)
+
+		length -= length_from
+		print(i, diff/math.pi, "pi", length, length_from)
+		pos = [poly[i][0] + math.cos(angle) * (length / 2 + length_from), poly[i][1] + math.sin(angle) * (length / 2 + length_from), 0.5]
+		walls.append(create_wall(pos, orientation, length, width))
+		prev_angle = angle
+	return walls
 
 
-def create_map(p, in_map, epsilon):
+def create_map(in_map, epsilon):
+	walls = []
 	for poly in in_map:
-		create_poly_wall(p, poly, epsilon)
+		walls += create_poly_wall(poly, epsilon)
+	return walls
 
 def main():
 
@@ -74,8 +107,8 @@ def main():
 	targetVelocitySlider = p.addUserDebugParameter("wheelVelocity", -30, 30, 0)
 	maxForceSlider = p.addUserDebugParameter("maxForce", 0, 10, 10)
 	steeringSlider = p.addUserDebugParameter("steering", -1, 1, 0)
-	epsilon = 0.01
-	create_poly_wall(p, [(-1, 3), (0, 7), (1, 3), (5, 5), (3, 1), (7, 0), (3,-1), (5, -5), (1, -3), (0, -7), (-1, -3), (-5, -5), (-3, -1), (-7, 0), (-3, 1), (-5, 5), (-1, 3)], epsilon)
+	epsilon = 0.1
+	create_poly_wall([(-1, 3), (0, 10), (1, 3), (10, 10), (3, 1), (10, 0), (3,-1), (10, -10), (1, -3), (0, -10), (-1, -3), (-10, -10), (-3, -1), (-10, 0), (-3, 1), (-10, 10), (-2, 4)], epsilon)
 
 	while True:
 	    maxForce = p.readUserDebugParameter(maxForceSlider)
@@ -96,8 +129,8 @@ def main():
 	        p.setJointMotorControl2(
 	            car, steer, p.POSITION_CONTROL, targetPosition=steeringAngle
 	        )
-	    if useRealTimeSim == 0:
-	        p.stepSimulation()
+	    #if useRealTimeSim == 0:
+	    #    p.stepSimulation()
 	    # time.sleep(0.01)
 		## first ball
 
