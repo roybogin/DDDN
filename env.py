37f6edc8eec7bb0ec317435348b11aa7f7721c51
roybogin <roybog00@gmail.com>
2022-08-04 17:39:12 +0300

    better formula?

--- env.py
+++ env.py
@@ -267,7 +267,7 @@ class CarEnv(gym.Env):
     def calculate_reward(self):
         reward = (
                 consts.TIME_PENALTY +
-                self.crushed * (consts.CRUSH_PENALTY + self.time * consts.TIME_PENALTY) +
+                self.crushed * consts.CRUSH_PENALTY * (1 - 0.5 * self.time/consts.max_time) +
                 self.finished * consts.FINISH_REWARD +
                 self.discovery_difference * consts.DISCOVER_REWARD +
                 (self.min_distance_to_target / self.initial_distance_to_target) * consts.MIN_DIST_PENALTY
