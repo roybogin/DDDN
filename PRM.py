3767d9b6d2c938b6b858e1bee45ae62fe92530ef
DCH <101271235+DCH0077@users.noreply.github.com>
2022-08-25 19:24:06 +0300

    Update PRM.py

--- PRM.py
+++ PRM.py
@@ -164,8 +164,9 @@ class PRM:
                     new_vertex = self.add_vertex(np.array([x, y]), theta, block=(row_idx, col_idx))
                     self.vertices_by_blocks[(row_idx, col_idx)].append(new_vertex)
                     count += 1
-            with open(consts.graph_file, 'wb') as f:
-                pickle.dump(self, f)
+            if row_id % 5 == 0:
+                with open(consts.graph_file, 'wb') as f:
+                    pickle.dump(self, f)
 
     def try_add_edge(self, v_1: Vertex, v_2: Vertex, angle_matters: bool = True):
         weight = dist(v_1.pos, v_2.pos)
