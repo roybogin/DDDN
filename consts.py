5c54a43dd33b5e0af539da8667acaeae834fee1f
DCH <dchuparov@gmail.com>
2022-08-28 16:43:19 +0300

    small bug fix

--- consts.py
+++ consts.py
@@ -1,5 +1,6 @@
 # pybullet simulation
 from gym.utils import seeding
+import math
 
 is_visual = False  # do we want to see visual footage
 use_real_time = 0  # is the simulation running in real time - probably should always be 0
@@ -22,7 +23,7 @@ min_dist_to_target = 0.5  # distance from target that is treated as success
 ray_length = 5  # length of ray
 ray_amount = 6
 size_map_quarter = 10
-vertex_offset = 0.1
+vertex_offset = 0.12
 map_borders = [
     (size_map_quarter, size_map_quarter),
     (size_map_quarter, -size_map_quarter),
@@ -35,7 +36,7 @@ seed = None  # randomness seed
 
 
 directions_per_vertex = 36
-amount_vertices_from_edge = 3
+amount_vertices_from_edge = math.ceil(0.3 / vertex_offset)
 
 max_hits_before_calculation = 10  # amounts of new hits before adding lines to the map
 max_time = int(1.5e4)  # time before forcing a new maze
